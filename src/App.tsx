8c7d2d39e26c226244bf0239abf9161ea595f768
glorious22 <gloryngombo22@gmail.com>
2025-06-20 10:38:38 +0200

    Fix Missing Service Page Imports

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,6 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Gallery from "./pages/Gallery";
 
-// Service sub-pages
-import HookahLounge from "./pages/services/HookahLounge";
-import PrivateBookings from "./pages/services/PrivateBookings";
-import LiveEntertainment from "./pages/services/LiveEntertainment";
-import PreOrderFood from "./pages/services/PreOrderFood";
-import Perfumes from "./pages/services/Perfumes";
-import BarberNextDoor from "./pages/services/BarberNextDoor";
-import GameNights from "./pages/services/GameNights";
-
 // Menu sub-pages
 import DailySpecials from "./pages/menu/DailySpecials";
 import VegetableSides from "./pages/menu/VegetableSides";
@@ -39,13 +30,6 @@ export default () => (
           <Route path="/menu/vegetable-sides" element={<VegetableSides />} />
           <Route path="/menu/sides" element={<Sides />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/services/hookah-lounge" element={<HookahLounge />} />
-          <Route path="/services/private-bookings" element={<PrivateBookings />} />
-          <Route path="/services/live-entertainment" element={<LiveEntertainment />} />
-          <Route path="/services/pre-order-food" element={<PreOrderFood />} />
-          <Route path="/services/perfumes" element={<Perfumes />} />
-          <Route path="/services/barber-next-door" element={<BarberNextDoor />} />
-          <Route path="/services/game-nights" element={<GameNights />} />
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/reservations" element={<Reservations />} />
           <Route path="/about" element={<About />} />
